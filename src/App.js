08b1c634944343adb89347221120ce1cae79d0e8
mauriciosierrac <2376@holbertonschool.com>
2021-08-04 14:48:59 -0500

    update styles in front

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import ReactDOM from 'react-dom';
 import SearchArea from './SearchArea';
 import { Router } from '@reach/router'
 import WatchArea from './WatchArea';
+import Logo from './images/logoMao.jpg'
 
 const App = () => {
     return (
         <div>
-            {/* <header>
+            <header>
                 <a href="/">
+                    <img src={Logo} alt="Logo" />
+                    Video Browser
                 </a>
-            </header> */}
-            <img src="logoMao.jpg" alt="Logo"/>
+            </header>
             <Router>
                 <SearchArea path="/" />
                 <WatchArea path="/watch/:id" />
             </Router>
 
         </div>
-            )
+    )
 
-            };
-            ReactDOM.render(React.createElement(App), document.getElementById("root"));
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
